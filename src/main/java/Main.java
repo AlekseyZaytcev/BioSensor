e9806d42182a453511f9c1c84356d897e0bfb96a
AlekseyZaytcev <aleksey2142.zaytcev@gmail.com>
2017-07-19 18:48:17 +0300

    Add fxml LineChart exemple.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,25 @@
-import dao.DaoManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.hibernate.query.Query;
+
 
 public class Main extends Application {
-
-    public static void main(String[] args) throws Exception {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+            Scene scene = new Scene(root);
+//            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        String fxmlFile = "/fxml/main.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(new Scene(root));
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,10 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 
-/**
- * Created by root on 19.07.2017.
- */
 public class MainController {
+    @FXML LineChart<String, Number> lineChart;
 
+    public void btn(ActionEvent event) {
+        lineChart.getData().clear();
+        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
+        series.getData().add(new XYChart.Data<String, Number>("Jan", 200));
+        series.getData().add(new XYChart.Data<String, Number>("Feb", 500));
+        series.getData().add(new XYChart.Data<String, Number>("Mar", 300));
+        series.getData().add(new XYChart.Data<String, Number>("Apr", 600));
+        series.setName("Month Pay");
+        lineChart.getData().add(series);
+    }
 }
